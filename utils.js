95c7889cd32d8cff65b7a94b7b30dab1264607b1
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-05 15:39:41 -0700

    created local storage functions

---
+++ utils.js
@@ -0,0 +1,10 @@
+import pokeData from './data.js';
+
+// Utilize findById func from last lab
+export function findById(array, id) {
+    return array.find(item => id === item.id);
+}
+
+export function findByPokeName(name) {
+    return pokeData.find(poke => name === poke.name);
+}
